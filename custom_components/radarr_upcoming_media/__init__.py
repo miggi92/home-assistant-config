ac70e38076a8a242fc266454d1674dfe00f2da52
miggi92 <miggi92@users.noreply.github.com>
2025-07-25 12:33:35 +0200

    chore: :arrow_up: upgrade custom components

--- custom_components/radarr_upcoming_media/__init__.py
+++ custom_components/radarr_upcoming_media/__init__.py
@@ -6,26 +6,27 @@ from homeassistant.const import (
     CONF_API_KEY,
     CONF_HOST,
     CONF_PORT,
-    CONF_SSL
+    CONF_SSL,
     )
 
 from .const import (
     DOMAIN,
     CONF_DAYS,
     CONF_URLBASE,
+    CONF_THEATERS,
     CONF_MAX,
 )
-from .coordinator import SonarrDataCoordinator
+from .coordinator import RadarrDataCoordinator
 from .helpers import setup_client
-from .sonarr_api import (
+from .radarr_api import (
     FailedToLogin,
-    SonarrCannotBeReached
+    RadarrCannotBeReached
 )
 from .parsing import TMDBApiNotResponding
 
-PLATFORMS = [
+PLATFORMS = {
     Platform.SENSOR
-]
+}
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     try:
@@ -38,25 +39,27 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
             config_entry.data[CONF_PORT],
             config_entry.data[CONF_SSL],
             config_entry.data[CONF_URLBASE],
+            config_entry.data[CONF_THEATERS],
             config_entry.data[CONF_MAX],
         )
     except FailedToLogin as err:
-        raise ConfigEntryNotReady("Failed to Log-in") from err
-    except SonarrCannotBeReached as err:
-        raise ConfigEntryNotReady("Sonarr cannot be reached") from err
+        raise ConfigEntryNotReady('Failed to Log-in') from err
+    except RadarrCannotBeReached as err:
+        raise ConfigEntryNotReady('Radarr cannot be reached') from err
     except TMDBApiNotResponding as err:
-        raise ConfigEntryNotReady("TMDB API is not responding") from err
-    coordinator = SonarrDataCoordinator(hass, client)
+        raise ConfigEntryNotReady('TMDB API is not responding') from err
+    coordinator = RadarrDataCoordinator(hass, client)
 
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    config_entry.async_on_unload(config_entry.add_update_listener(update_listener))
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """Unload Sonarr config entry."""
+    """Unload Radarr config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(
         config_entry, PLATFORMS
     ):
@@ -64,3 +67,7 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
         if not hass.data[DOMAIN]:
             del hass.data[DOMAIN]
     return unload_ok
+
+async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
