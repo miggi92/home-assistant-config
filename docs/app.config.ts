f1805a5a0a4a5f9008bd2ddaa31b7419b345d632
miggi92 <miggi92@users.noreply.github.com>
2025-12-02 20:18:22 +0100

    feat: :sparkles: updated colors

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,4 +1,10 @@
 export default defineAppConfig({
+    ui: {
+        colors: {
+            primary: 'sky',
+            neutral: 'slate',
+        },
+    },
     github: {
         url: 'https://github.com/miggi92/home-assistant-config',
         branch: 'master',
