7e8f98e87b1938e96e898725bc0ccf753952d4ab
miggi92 <miggi92@users.noreply.github.com>
2025-12-02 20:25:26 +0100

    feat: :lipstick: updated ui

---
+++ docs/components/AppHeaderLogo.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <img src="/logo.jpg" alt="App Logo" class="h-8 w-8 rounded-md">
+    <h1>MGM12 Home Assistant Config</h1>
+</template>
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     'nuxt-studio'
   ],
+  css: ['~/assets/css/main.css'],
   i18n: {
     defaultLocale: 'en',
     locales: [{
